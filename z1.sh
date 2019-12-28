893b54d22893b892447a53aeb293558d80c9889f
kongkrit <45794368+kongkrit@users.noreply.github.com>
2019-12-28 09:12:04 +0000

    Update z1.sh

--- z1.sh
+++ z1.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo === add universe repo ===
+echo "=== 1.2 adding universe repo and update"
 sudo apt-add-repository universe
 sudo apt update
+echo "=== 1.3 install openssh-server"
 sudo apt install -y openssh-server
-echo =======================
-echo === note IP address ===
-echo =======================
+echo "======================="
+echo "=== note IP address ==="
+echo "======================="
 ip a
-echo === changing password for ssh login
+echo "=== 1.3changing password for ssh login"
 passwd
