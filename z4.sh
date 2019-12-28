81f3df3e01622323196ad1d025ddac6c7a28727d
kongkrit <45794368+kongkrit@users.noreply.github.com>
2019-12-28 10:45:19 +0000

    Update z4.sh

--- z4.sh
+++ z4.sh
@@ -29,3 +29,32 @@ echo "network:
 #      nameservers:
 #              addresses: [8.8.8.8, 8.8.4.4]" >> /mnt/etc/netplan/01-netcfg.yaml
 echo "=== 4.2 wrote /mnt/etc/netplan/01-netcfg.yaml"
+
+echo "=== 4.3 Configure the package sources in /mnt/etc/apt/sources.list:"
+echo "# ubuntu archives
+deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu bionic main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu bionic-backports main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu bionic-backports main restricted universe multiverse
+deb http://security.ubuntu.com/ubuntu bionic-security main restricted universe multiverse" \
+ >> /mnt/etc/apt/sources.list
+
+echo "=== 4.4 Bind the virtual filesystems from the LiveCD environment to the new system"
+mount --rbind /dev  /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --rbind /sys  /mnt/sys
+
+echo "=== 4.4 listing disks"
+ls -alF /dev/disk/by-id/
+echo "=== 4.4 disks list above"
+read -p "=== enter name of disk1 of 2:" DISK
+DISK=/dev/disk/by-id/$DISK
+echo "==> disk1 is $DISK"
+read -p "=== enter name of disk2 of 2:" DISK2
+DISK2=/dev/disk/by-id/$DISK2
+
+echo "=== 4.4 doing chroot"
+chroot /mnt /usr/bin/env DISK=$DISK DISK2=$DISK2 bash --login
+echo "=== 4.4 done chroot"
