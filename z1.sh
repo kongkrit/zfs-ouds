5aa2059cc783776c30520f80e41555e6dc0d58fd
kongkrit <45794368+kongkrit@users.noreply.github.com>
2019-12-28 15:00:21 +0700

    initial commit

---
+++ z1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo *** add universe repo ***
+sudo apt-add-repository universe
+sudo apt update
+sudo apt install -y openssh-server
+echo ***********************
+echo *** note IP address ***
+echo ***********************
+ip a
+echo *** changing password for ssh login
+passwd
