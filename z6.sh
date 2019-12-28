664530153242f4dbfd44259025798b83ca50b828
kongkrit <45794368+kongkrit@users.noreply.github.com>
2019-12-29 00:23:54 +0700

    Update z6.sh

--- z6.sh
+++ z6.sh
@@ -11,3 +11,8 @@ zfs create rpool/home/$USERNAME
 adduser $USERNAME
 cp -a /etc/skel/. /home/$USERNAME
 chown -R $USERNAME:$USERNAME /home/$USERNAME
+
+echo "=== 6.7 Add [$USERNAME] to the default set groups for an administrator:"
+usermod -a -G adm,cdrom,dip,lpadmin,plugdev,sambashare,sudo YOURUSERNAME
+
+echo "=== 6.7 done -- continue with z6b.sh"
