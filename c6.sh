6fb5253c5b1826e7c806d2ed2a4a1adcbd6bdfff
kongkrit <45794368+kongkrit@users.noreply.github.com>
2019-12-29 10:51:31 +0700

    Create c6.sh

---
+++ c6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $USER != "root" ]; then
+        echo "must run as root"
+        exit -1
+fi
+
+echo "=== 6.6 Create a user account:"
+read -p "   enter username:" USERNAME
+zfs create rpool/home/$USERNAME
+adduser $USERNAME
+cp -a /etc/skel/. /home/$USERNAME
+chown -R $USERNAME:$USERNAME /home/$USERNAME
+
+echo "=== 6.7 Add [$USERNAME] to the default set groups for an administrator:"
+usermod -a -G adm,cdrom,dip,lpadmin,plugdev,sambashare,sudo $USERNAME
+
+echo "=== 6.7 done -- continue with c6b.sh -- from ssh ${USERNAME}@ipaddress"
