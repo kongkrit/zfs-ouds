b1260e4a63eeeb590e7b94f3d750dc61bc832ee1
kongkrit <45794368+kongkrit@users.noreply.github.com>
2020-01-28 14:16:11 +0700

    Update a1.sh

--- a1.sh
+++ a1.sh
@@ -117,71 +117,60 @@ apt install --yes debootstrap gdisk
 echo "=== 2.2 formatting both disks"
 sgdisk --zap-all $DISK
 sgdisk --zap-all $DISK2
-echo "=== 2.3 creating paritions for UEFI booting"
-sgdisk -n2:1M:+512M -t2:EF00 $DISK
-sgdisk -n2:1M:+512M -t2:EF00 $DISK2
-echo "=== 2.3 creating boot partitions (UEFI)"
-sgdisk -n3:0:+1G -t3:BF01 $DISK
-sgdisk -n3:0:+1G -t3:BF01 $DISK2
-echo "=== 2.3a creating root partitions (unencrypted)"
-sgdisk     -n4:0:0        -t4:BF01 $DISK
-sgdisk     -n4:0:0        -t4:BF01 $DISK2
+echo "=== 2.3 creating paritions for UEFI booting (EFI partition)"
+sgdisk -n1:1M:+512M -t2:EF00 $DISK
+sgdisk -n1:1M:+512M -t2:EF00 $DISK2
+echo "=== 2.3 creating boot pool"
+sgdisk -n2:0:+512M -t3:BF01 $DISK
+sgdisk -n2:0:+512M -t3:BF01 $DISK2
+echo "=== 2.3a creating root pool (unencrypted)"
+sgdisk     -n3:0:0        -t4:BF01 $DISK
+sgdisk     -n3:0:0        -t4:BF01 $DISK2
 
-echo "=== 2.3a done, sleeping for 3 seconds"
+echo "=== 2.3a done, issue udevadm settle"
+udevadm settle
+echo "=== 2.3a udevadm settle done, sleeping for 3 seconds"
 sleep 3
 
+echo "=== 2.3b create FAT for EFI partitions"
+mkfs.fat -F 32 -n EFI "${DISK}-part1"
+mkfs.fat -F 32 -n EFI "${DISK2}-part1"
+
 echo "=== 2.4 creating boot pool (mirrored)"
+#zpool create -o ashift=13 \
+#    -O acltype=posixacl -O canmount=off -O compression=lz4 -O atime=off -O xattr=sa \
+#    -O mountpoint=/ -R /mnt bpool mirror ${DISK}-part2 ${DISK2}-part2
 zpool create -o ashift=13 \
-    -O acltype=posixacl -O canmount=off -O compression=lz4 -O atime=off -O xattr=sa \
-    -O mountpoint=/ -R /mnt bpool mirror ${DISK}-part3 ${DISK2}-part3
+    -O acltype=posixacl -O compression=lz4 -O atime=off -O xattr=sa \
+    -O devices=off -O mountpoint=/boot \
+    -R /mnt -f bpool mirror ${DISK}-part2 ${DISK2}-part2
 
 echo "=== 2.5a creating root pool mirror (unencrypted)"
+#zpool create -o ashift=13 \
+#    -O acltype=posixacl -O canmount=off -O compression=lz4 \
+#    -O dnodesize=auto -O normalization=formD -O atime=off -O xattr=sa \
+#    -O mountpoint=/ -R /mnt rpool mirror ${DISK}-part3 ${DISK2}-part3
 zpool create -o ashift=13 \
-    -O acltype=posixacl -O canmount=off -O compression=lz4 \
+    -O acltype=posixacl -O compression=lz4 \
     -O dnodesize=auto -O normalization=formD -O atime=off -O xattr=sa \
-    -O mountpoint=/ -R /mnt rpool mirror ${DISK}-part4 ${DISK2}-part4
+    -O devices=off -O mountpoint=/ -R /mnt rpool mirror ${DISK}-part3 ${DISK2}-part3
 
-echo "=== 3.1 Create filesystem datasets to act as containers"
-zfs create -o canmount=off -o mountpoint=none rpool/ROOT
-zfs create -o canmount=off -o mountpoint=none bpool/BOOT
+#echo "=== 3.1 Create filesystem datasets to act as containers"
+#zfs create -o canmount=off -o mountpoint=none rpool/ROOT
+#zfs create -o canmount=off -o mountpoint=none bpool/BOOT
 
-echo "=== 3.2 Create filesystem datasets for the root and boot filesystems:"
-zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
-zfs mount rpool/ROOT/ubuntu
-zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/ubuntu
-zfs mount bpool/BOOT/ubuntu
+#echo "=== 3.2 Create filesystem datasets for the root and boot filesystems:"
+#zfs create -o canmount=noauto -o mountpoint=/ rpool/ROOT/ubuntu
+#zfs mount rpool/ROOT/ubuntu
+#zfs create -o canmount=noauto -o mountpoint=/boot bpool/BOOT/ubuntu
+#zfs mount bpool/BOOT/ubuntu
+zfs mount bpool
+zfs mount rpool
 
 #echo "=== 3.3 Create datasets:"
-#zfs create  rpool/home
-#zfs create -o mountpoint=/root rpool/home/root
-#zfs create -o canmount=off rpool/var
-#zfs create -o canmount=off rpool/var/lib
-#zfs create rpool/var/log
-#zfs create rpool/var/spool
-
-#echo "=== 3.3 exclude /var/tmp /var/cache from snapshots"
-#zfs create -o com.sun:auto-snapshot=false  rpool/var/cache
-#zfs create -o com.sun:auto-snapshot=false  rpool/var/tmp
-#chmod 1777 /mnt/var/tmp
-
+#echo "=== 3.3 exclude /var/tmp /var/cache from snapshots"1
 #echo "=== 3.3 create /opt /srv /usr /usr/local /var/games"
 #echo "===     /var/mail /var/snap /var/www docker nfs tmpfs"
-#zfs create                                 rpool/opt
-#zfs create                                 rpool/srv
-#zfs create -o canmount=off                 rpool/usr
-#zfs create                                 rpool/usr/local
-#zfs create                                 rpool/var/games
-#store local email
-#zfs create                                 rpool/var/mail
-#zfs create                                 rpool/var/snap
-#zfs create                                 rpool/var/www
-#if you use GNOME
-#zfs create                                 rpool/var/lib/AccountsService
-#zfs create -o com.sun:auto-snapshot=false  rpool/var/lib/docker
-#zfs create -o com.sun:auto-snapshot=false  rpool/var/lib/nfs
-# tmpfs
-#zfs create -o com.sun:auto-snapshot=false  rpool/tmp
-#chmod 1777 /mnt/tmp
 
 echo "=== 3.4 Install the minimal system:"
 echo "=== 3.4 debootstrap to /mnt"
